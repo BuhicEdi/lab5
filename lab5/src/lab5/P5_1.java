2421c7c22b4d9f96c89c6e254013b8ea04bcde44
unknown <edibuh-2@student.ltu.se>
2024-09-30 15:25:38 +0200

    Lab 5

---
+++ lab5/src/lab5/P5_1.java
@@ -0,0 +1,22 @@
+package lab5;
+
+public class P5_1 {
+
+	public static void main(String[] args) {
+		PrintArray pr = new PrintArray();
+		int[] array = pr.getArray();
+		
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		
+		System.out.println();
+		pr.printReverse();
+		
+		
+		
+		
+	}
+
+}
