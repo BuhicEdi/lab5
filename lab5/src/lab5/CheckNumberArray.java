d3aee7ec663d6c529ff2f2e570de846ccba60f64
unknown <edibuh-2@student.ltu.se>
2024-10-01 17:25:19 +0200

    Lab 5

---
+++ lab5/src/lab5/CheckNumberArray.java
@@ -0,0 +1,82 @@
+package lab5;
+
+public class CheckNumberArray {
+
+	private static final int MAX_ELEMENTS = 10;
+	private int[] arrayOfInt;
+	private int nbrOfElements;
+
+	public CheckNumberArray() {
+		this.arrayOfInt = new int[10];
+	}
+
+	public void addNbr(int value) {
+		if (nbrOfElements < MAX_ELEMENTS) {
+			this.arrayOfInt[nbrOfElements] = value;
+			nbrOfElements++;
+		}
+	}
+
+	public int getNbrOfElements() {
+		return nbrOfElements;
+	}
+
+	public int getSum() {
+		int valueSum = 0;
+
+		for (int i = 0; i <= nbrOfElements; i++) {
+			valueSum += arrayOfInt[i];
+		}
+
+		return valueSum;
+	}
+
+	public double getAverage() {
+		double totalElements = nbrOfElements;
+
+		return getSum() / totalElements;
+	}
+
+	public boolean isAllTheSame() {
+		for (int i = 0; i < nbrOfElements; i++) {
+			if (arrayOfInt[i] != arrayOfInt[i + 1]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public boolean isAccOrder() {
+		for (int i = nbrOfElements - 1; i > 0; i--) {
+			if (arrayOfInt[i - 1] > arrayOfInt[i]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public boolean isDecOrder() {
+		for (int i = 0; i < nbrOfElements; i++) {
+			if (arrayOfInt[i + 1] > arrayOfInt[i]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public boolean isOrdered() {
+		if (isAccOrder() == true || isDecOrder() == true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+
+
+
+
+}
---
+++ lab5/src/lab5/P5_2.java
@@ -0,0 +1,21 @@
+package lab5;
+
+public class P5_2 {
+
+	public static void main(String[] args) {
+		CheckNumberArray arr = new CheckNumberArray();
+		
+		arr.addNbr(2);
+		arr.addNbr(2);
+		arr.addNbr(2);
+		
+		
+		System.out.println(arr.getNbrOfElements());
+		System.out.println(arr.getSum());
+		System.out.println(arr.isAllTheSame());
+		System.out.println(arr.isAccOrder());
+		System.out.println(arr.isDecOrder());
+		System.out.println(arr.isOrdered());
+	}
+
+}
