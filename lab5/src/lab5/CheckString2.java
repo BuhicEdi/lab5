d2d5b428774f06245f14b4ccbce6d50cd458921f
unknown <edibuh-2@student.ltu.se>
2024-10-02 14:02:41 +0200

    Lab 5

--- lab5/src/lab5/CheckString2.java
+++ lab5/src/lab5/CheckString2.java
@@ -49,8 +49,7 @@ public class CheckString2 {
 	}
 
 	public int countWords() {
-		int wordCount = 0;
-		
+		int wordCount = 1;
 		
 		for (int i = 0; i > str.length(); i++) {
 			if (str.substring(i, i + 1) == " ") {
