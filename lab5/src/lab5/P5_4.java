7a81dd5e940dfcf75df00c968dcb5cd9701abeca
unknown <edibuh-2@student.ltu.se>
2024-10-02 13:32:54 +0200

    Lab 5

--- lab5/src/lab5/P5_4.java
+++ lab5/src/lab5/P5_4.java
@@ -3,11 +3,11 @@ package lab5;
 public class P5_4 {
 
 	public static void main(String[] args) {
-		CheckString2 c = new CheckString2("abcdefghijklmnop");
+		CheckString2 c = new CheckString2("1");
 		
 		
 		System.out.println(c.middle());
-		System.out.println(c.findString("ghij"));
+		System.out.println(c.findString("1"));
 	}
 
 }
