9f6c23f8a60b68b96ed67ba4bbfb6c6f42b38015
unknown <edibuh-2@student.ltu.se>
2024-10-02 09:47:40 +0200

    Lab 5

---
+++ lab5/src/lab5/ManipulateArray.java
@@ -0,0 +1,45 @@
+package lab5;
+
+import java.util.Random;
+
+public class ManipulateArray {
+
+	private int[] arrayOfInt;
+	
+	
+	public ManipulateArray() {
+		
+		Random r = new Random();
+		this.arrayOfInt = new int[10];
+		
+		for (int i = 0; i < 10; i++) {
+			this.arrayOfInt[i] = r.nextInt(1, 10);
+		}
+	}
+	
+	public ManipulateArray(int[] arr) {
+		this.arrayOfInt = arr;
+	}
+	
+	public int[] getArrayCopy() {
+		return arrayOfInt.clone();
+	}
+	
+	public int getArraySize() {
+		return arrayOfInt.length;
+	}
+	
+	public String swapFirstAndLast() {
+		int[] arrSwap = arrayOfInt.clone();
+		int last = arrayOfInt.length - 1;
+		
+		arrSwap[0] = arrayOfInt[last];
+		arrSwap[last] = arrayOfInt[0];
+		
+		return arrSwap.toString();
+	}
+	
+	
+	
+	
+}
