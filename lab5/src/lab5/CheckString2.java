cbeea7324c9d83df7469d1f06a51c2a3a1533acf
unknown <edibuh-2@student.ltu.se>
2024-10-02 11:58:59 +0200

    Lab 5

---
+++ lab5/src/lab5/CheckString2.java
@@ -0,0 +1,54 @@
+package lab5;
+
+public class CheckString2 {
+	
+	private String str;
+	
+	public CheckString2(String str) {
+		this.str = str;
+	}
+	
+	public String getString() {
+		return str;
+	}
+	
+	public String middle() {
+		String midStr;
+		
+		if (str.length() %2 == 0) {
+			midStr = str.substring((str.length()/2 - 1), (str.length()/2) + 1);
+		} else {
+			midStr = str.substring((str.length()/2), (str.length()/2) + 1);
+		}
+		
+		return midStr;
+	}
+	
+	public int countVowels() {
+		int voCount = 0;
+		String vowels = "aoueiyåäöAOUEIYÅÄÖ";
+		
+		for (int i = 0; i < str.length(); i++) {
+			if (vowels.contains(str.substring(i, i + 1))) {
+				voCount++;
+			}
+		}
+		
+		return voCount;
+	}
+	
+	public int findString(String strSearch) {
+		for(int i = 0; i < str.length(); i = i + strSearch.length()) {
+			if (strSearch.contains(str.substring(i, i + strSearch.length()))) {
+				return str.indexOf(strSearch);
+			}
+		}
+		
+		return -1;
+	}
+	
+	public int countWords() {
+		return 0;
+	}
+	
+}
