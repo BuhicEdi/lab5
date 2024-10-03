e0db2b410a2c8602113dfc44e65bc91c2f680adb
unknown <edibuh-2@student.ltu.se>
2024-10-03 15:16:16 +0200

    Lab 5

--- lab5/src/lab5/ManipulateString.java
+++ lab5/src/lab5/ManipulateString.java
@@ -20,7 +20,7 @@ public class ManipulateString {
 	} 
 	
 	public String reverse() {
-		String strReversed = null;
+		String strReversed = "";
 		
 		for (int i = str.length() - 1; i >= 0; i--) {
 			strReversed += str.charAt(i);
