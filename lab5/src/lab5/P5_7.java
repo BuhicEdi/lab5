5588acf0d39ddb7d0a9fb630660f66b5a31426f5
unknown <edibuh-2@student.ltu.se>
2024-10-06 21:20:04 +0200

    Lab 5

--- lab5/src/lab5/P5_7.java
+++ lab5/src/lab5/P5_7.java
@@ -5,7 +5,7 @@ public class P5_7 {
 	public static void main(String[] args) {
 		Student2 newStud = new Student2(args[0]);
 
-		for (int i = 1; i <= args.length; i++) {
+		for (int i = 1; i < args.length; i++) {
 			newStud.addQuizScore(Double.valueOf(args[i]));
 		}
 
